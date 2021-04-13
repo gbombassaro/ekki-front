0dd5d148389052532453613bcfcc408b05059507
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-04-12 22:16:58 -0300

    Monta tela de transação

--- src/pages/NewTransaction.js
+++ src/pages/NewTransaction.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Header from '../components/header';
+import Section from '../components/section';
+import TransactionForm from '../components/form';
+import UserForm from '../components/userList';
 
 const NewTransaction = props => {
-  return <pre>transferir</pre>
+  const {data} = props;
+  const {beneficiaryList} = data;
+
+  const [page, setPage] = useState(0);
+  const [transactionDestiny, setTransactionDestiny] = useState('');
+  const [transactionValue, setTransactionValue] = useState(0);
+
+  const transactionFormData = [
+    {
+      id: 'value',
+      value: transactionValue,
+      onChange: setTransactionValue,
+    }
+  ]
+  const headerActionButtons = [
+    {
+      id: 'goback',
+      children: 'Voltar para a home',
+      link: '/home',
+    }
+  ]
+
+  const handleDestinySelection = data => {
+    setTransactionDestiny(data.id);
+    setPage(1);
+  }
+
+  const executeTransaction = () => {
+    console.log('execute');
+  }
+
+  return (
+    <React.Fragment>
+      <Header title='Transferir' actionButtons={headerActionButtons} />
+      {page === 0 &&
+        <Section justifyContent='center'>
+          <UserForm
+            data={beneficiaryList}
+            onClick={handleDestinySelection}
+            title='Para quem será a transferência?'
+            primaryInformation='name'
+            secondaryInformation='cpf'
+          />
+        </Section>
+      }
+      {page === 1 &&
+        <Section justifyContent='center'>
+          <TransactionForm
+            data={transactionFormData}
+            primaryButtonAction={executeTransaction}
+            secondaryButtonAction={() => setPage(0)}
+            title='Qual o valor?'
+            primaryButtonText='Transferir'
+            secondaryButtonText='Voltar'
+          />
+        </Section>
+      }
+    </React.Fragment>
+  )
 }
 
 export default NewTransaction;
