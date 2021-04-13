4e5cf62e724abdb8fa36b792d01124acca54bc8f
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-04-12 23:59:56 -0300

    Adiciona disabled no componente Form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -11,20 +11,22 @@ const Form = props => {
     data,
     primaryButtonAction,
     primaryButtonText,
+    disablePrimaryButton,
     secondaryButtonAction,
     secondaryButtonText,
+    disableSecondaryButton,
     title
   } = props;
 
   return (
     <Box width='100%' flexDirection='column' maxWidth={600}>
       <h3>{title}</h3>
-      <Box width='100%'>
+      <Box width='100%' flexDirection='column'>
         {map(data, entry => <Input key={entry.id} {...entry} value={entry.value} onChange={entry.onChange} />)}
       </Box>
-      <Box width='100%' justifyContent='space-between' marginTop={3}>
-        {secondaryButtonAction && <Button onClick={secondaryButtonAction}>{secondaryButtonText}</Button>}
-        {primaryButtonAction && <Button onClick={primaryButtonAction} hasIcon>{primaryButtonText}</Button>}
+      <Box width='100%' justifyContent={secondaryButtonAction ? 'space-between' : 'flex-end'} marginTop={3}>
+        {secondaryButtonAction && <Button disabled={disableSecondaryButton} onClick={secondaryButtonAction}>{secondaryButtonText}</Button>}
+        {primaryButtonAction && <Button disabled={disablePrimaryButton} onClick={primaryButtonAction} hasIcon>{primaryButtonText}</Button>}
       </Box>
     </Box>
   )
@@ -34,8 +36,10 @@ Form.propTypes = {
   data: PropTypes.array.isRequired,
   primaryButtonAction: PropTypes.func,
   primaryButtonText: PropTypes.string,
+  disablePrimaryButton: PropTypes.bool,
   secondaryButtonAction: PropTypes.func,
   secondaryButtonText: PropTypes.string,
+  disableSecondaryButton: PropTypes.bool,
   title: PropTypes.string,
 }
 
